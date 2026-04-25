743c8166f532c8d259bcf39264efc7e613e2e67b
Deven5945 <devers5945@gmail.com>
2026-04-25 13:40:07 +0900

    Update quadratic_cmath.py

--- quadratic_cmath.py
+++ quadratic_cmath.py
@@ -1,34 +1,40 @@
-import cmath
-import math
+import ast
+import cmath #for sqrt and imaginary numbers
 
-def getNumber(number): #숫자 입력
-    while True: #숫자 입력할때까지 반복
+def getNumber(prompt): #getting a number
+    while True:
         try:
-            return complex(eval(input(number))) #입력값 유리수 변환(유리수 맞나 암튼 까먹음)
-        except (ValueError, NameError): #오류나면(숫자 아니면)
-            print("insert a number") #숫자 아니면 경고
+            return complex(ast.literal_eval(input(prompt))) #allows user to input sqrt or imaginary numbers, which is very very good good
+        except Exception: #if sth happens
+            print("error: wtf u did that for?") #you sob, you broke the program
 
+def is_integer(z): #is it integer
+    return z.imag == 0 and z.real == int(z.real) #yup or nope
+
+#getting numbers
 print("ax^2 + bx + c = 0")
-a = getNumber("a: ")
+a = getNumber("a: ") 
 b = getNumber("b: ")
 c = getNumber("c: ")
 
 if a == 0:
-    print("a cannot be 0") #a=0이면 빠꾸
+    print("a cannot be 0") #wtf
 else:
-    discrim = b**2 - 4*a*c #판별식 b^2 - 4ac
-
-    if discrim == 0:
-        root = -b / (2*a) #중근
-        print(f"real multiple root: {root}") #판별식 0이면 중근
+    discrim = b**2 - 4*a*c #discrimination
+    sqrt_discrim = cmath.sqrt(discrim) #sqrt
+    divisor = 2*a #i dont think it is necessary. why tf i made this
+    
+    if discrim == 0: #multiple root
+        root = -b / divisor
+        print(f"real multiple root: {root}")
     else:
-        root1 = (-b + cmath.sqrt(discrim)) / (2*a) #근1
-        root2 = (-b - cmath.sqrt(discrim)) / (2*a) #근2
+        root1 = (-b + sqrt_discrim) / divisor
+        root2 = (-b - sqrt_discrim) / divisor
         
-        print(f"first root: {-b / (2*a)} + sqrt({discrim}) / {(2*a)}")
-        print(f"second root: {-b / (2*a)} - sqrt({discrim}) / {(2*a)}")
+        print(f"first root: {-b / divisor} + sqrt({discrim}) / {divisor}")
+        print(f"second root: {-b / divisor} - sqrt({discrim}) / {divisor}")
         
-        if (root1.imag == 0 and root1.real % 1 == 0) and (root2.imag == 0 and root2.real % 1 == 0): #정수 실근
-            print(f"two real roots: {int(root1.real)}, {int(root2.real)}")
+        if is_integer(root1) and is_integer(root2):
+            print(f"two real roots: {int(root1.real)}, {int(root2.real)}") #if they are integers, print them.
         else:
-            print(f"roots: {root1}, {root2}") #정수 아니면
+            print(f"roots: {root1}, {root2}") #otherwise, print them as they are, such as complex numbers or imaginary numbers
